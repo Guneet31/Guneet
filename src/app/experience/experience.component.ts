23cbe3b162a5f1398ef1b03e3024487f3951091f
Guneet Singh Lamba <guneet@stablehacks.com>
2021-07-14 23:21:07 -0400

    changes for page v2

--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./experience.component.scss']
 })
 export class ExperienceComponent implements OnInit {
-
+  company1Description:String;
+  company1SubDescription:String;
+  company2Description:String;
+  company2SubDescription:String;
   constructor() { }
 
   ngOnInit(): void {
+    this.company1Description = "<p><li>Frontend development using Angular, TypeScript, iOS, Swift, Swift UI</li><li>Backend development using Loopback Node.js MySQL DB management and performing queries as per tasks</li><li> Deploy code on Google Cloud</li><li>Bitbucket for version control</li></p>"
+    this.company2Description = "<p><li>Frontend development using Angular, TypeScript</li><li>Backend development using Node.js, firebase</li><li> Performed frontend end testing using Selenium and Java</li><li> Deploy code on Jenkins, AWS Cloud</li><li>Bitbucket used for version control</li><p>"
+
+
   }
 
 }
